760d2607f5bfeaf40531143b1d5e3ca724577d7c
Matthew Cline <mdeancline@gmail.com>
2024-08-17 19:00:31 -0500

    Adjust visibility of internal EventListener handling

--- eventiful-plugin/src/main/java/io/github/eventiful/plugin/EventBusImpl.java
+++ eventiful-plugin/src/main/java/io/github/eventiful/plugin/EventBusImpl.java
@@ -2,25 +2,31 @@ package io.github.eventiful.plugin;
 
 import io.github.eventiful.api.EventToken;
 import io.github.eventiful.api.exception.EventConcurrencyException;
+import io.github.eventiful.api.exception.EventRegistrationException;
 import io.github.eventiful.api.listener.EventListener;
 import io.github.eventiful.plugin.registration.EventRegistration;
 import io.github.eventiful.plugin.registration.EventTokenProvider;
 import io.github.eventiful.plugin.registration.SimpleEventRegistration;
 import io.github.eventiful.plugin.scanner.ClassScanner;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
 
 @Setter
 @RequiredArgsConstructor
 public class EventBusImpl implements ServerEventBus {
-    private final Map<Class<?>, EventChannel<Event>> channels = new Object2ObjectOpenHashMap<>();
+    private final Map<Class<?>, Channel<Event>> channels = new Object2ObjectOpenHashMap<>();
     private final ClassScanner classScanner;
     private final EventLogger logger;
     private final EventTokenProvider tokenProvider;
@@ -44,7 +50,7 @@ public class EventBusImpl implements ServerEventBus {
     }
 
     private void publishToChannel(final Event event, final Class<?> type) {
-        final EventChannel<Event> channel = channels.get(type);
+        final Channel<Event> channel = channels.get(type);
         if (channel != null) channel.dispatch(event);
     }
 
@@ -63,13 +69,69 @@ public class EventBusImpl implements ServerEventBus {
 
     @SuppressWarnings("unchecked")
     private <T extends Event> EventToken registerAsTyped(final EventRegistration<T> registration) {
-        final EventChannel<T> channel = (EventChannel<T>) channels.computeIfAbsent(registration.getEventType(), k
-                -> new EventChannel<>(tokenProvider));
+        final Channel<T> channel = (Channel<T>) channels.computeIfAbsent(registration.getEventType(), k
+                -> new Channel<>(tokenProvider));
         return channel.register(registration);
     }
 
     @Override
     public void unregister(@NotNull final EventToken token) {
         channels.get(token.getType()).unregister(token);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static class Channel<T extends Event> {
+        private static final EventListener<?>[] DEFAULT_CACHE = new EventListener[0];
+
+        private final Map<EventToken, EventListener<T>> ownedListeners = new Object2ObjectOpenHashMap<>();
+        private final Map<EventPriority, List<EventListener<T>>> orderedListeners = new EnumMap<>(EventPriority.class);
+        private final EventTokenProvider tokenProvider;
+        private volatile EventListener<T>[] iterationCache = (EventListener<T>[]) DEFAULT_CACHE;
+
+        private Channel(final EventTokenProvider tokenProvider) {
+            this.tokenProvider = tokenProvider;
+
+            for (final EventPriority value : EventPriority.values())
+                orderedListeners.put(value, new ObjectArrayList<>());
+        }
+
+        public void dispatch(final T event) {
+            for (final EventListener<T> listener : iterationCache)
+                listener.handle(event);
+        }
+
+        public EventToken register(final EventRegistration<T> registration) {
+            final EventToken token = tokenProvider.createToken(registration);
+            final EventListener<T> listener = registration.getListener();
+            final EventPriority priority = registration.getPriority();
+
+            final List<EventListener<T>> listeners = orderedListeners.get(priority);
+            listeners.add(registration.getListener());
+            ownedListeners.put(token, listener);
+            refreshIterationCache(true);
+
+            return token;
+        }
+
+        public void unregister(final EventToken token) {
+            final EventListener<T> listener = ownedListeners.remove(token);
+            if (listener == null)
+                throw new EventRegistrationException("No EventListener under that EventToken is registered");
+
+            orderedListeners.get(token.getPriority()).remove(listener);
+            refreshIterationCache(false);
+        }
+
+        private void refreshIterationCache(final boolean increment) {
+            Executors.newSingleThreadExecutor().execute(() -> {
+                int length = iterationCache.length;
+                int i = 0;
+                iterationCache = new EventListener[increment ? ++length : --length];
+
+                for (final Map.Entry<EventPriority, List<EventListener<T>>> entry : orderedListeners.entrySet())
+                    for (final EventListener<T> listener : entry.getValue())
+                        iterationCache[i++] = listener;
+            });
+        }
     }
 }
