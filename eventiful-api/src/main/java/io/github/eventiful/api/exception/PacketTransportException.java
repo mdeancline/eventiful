9843f1d26c6cbbb292e490319dc2e25f4d2f7250
Matthew Cline <mdeancline@gmail.com>
2024-09-07 00:12:55 -0500

    Remove unthrown exceptions

--- eventiful-api/src/main/java/io/github/eventiful/api/exception/PacketTransportException.java
+++
@@ -1,9 +0,0 @@
-package io.github.eventiful.api.exception;
-
-public class PacketTransportException extends PacketException {
-    private static final long serialVersionUID = -850335848204931236L;
-
-    public PacketTransportException(final Exception cause) {
-        super(cause);
-    }
-}
